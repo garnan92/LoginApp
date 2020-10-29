9db0c810c2c1e325c3703c0aa2e345778c6fa751
Ranferi <ranferi.dr@gmail.com>
2020-10-28 19:43:44 -0600

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Ranferi Dominguez Rios on 28/10/20.
+//  Copyright © 2020 Ranferi Dominguez Rios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
